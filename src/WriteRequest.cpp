c16a5dc3611cc089b040d116cf155ef16a438b14
bongo505 <andrew.martina@knockanore.com>
2024-08-13 16:09:47 +1000

    essentially abandoned - protbuf causing stack issues on ESP32

--- src/WriteRequest.cpp
+++ src/WriteRequest.cpp
@@ -31,37 +31,38 @@ uint32_t WriteRequest::getBufferSize() {
 
 int16_t WriteRequest::toSnappyProto(uint8_t* output) {
     errmsg = nullptr;
-    log_d("Begin serialization: %d", ESP.getFreeHeap());
+    log_d("Req buffer: %d", _bufferSize);
+    log_d("Begin serialization: %d", ESP.getMinFreeHeap());
     uint8_t buffer[_bufferSize];
+    log_d("pb_ostream: %d", ESP.getMinFreeHeap());
     pb_ostream_t os = pb_ostream_from_buffer(buffer, sizeof(buffer));
 
+//    log_d("Series tuple: %d", 1);
     SeriesTuple st = SeriesTuple{
         series: _series,
         seriesCnt : _seriesPointer
     };
 
+//    log_d("RW: %d", 1);
     prometheus_WriteRequest rw = {};
     rw.timeseries.arg = &st;
     rw.timeseries.funcs.encode = &callback_encode_timeseries;
+//    log_d("Encode: %d", ESP.getFreeHeap());
     if (!pb_encode(&os, prometheus_WriteRequest_fields, &rw)) {
         log_d("Error from proto encode: %s", PB_GET_ERROR(&os));
         errmsg = (char*)"Error creating protobuf, enable debug logging to see more details";
         return -1;
     }
 
+    log_d("Buffer %d:\n%s", os.bytes_written, buffer);
     log_d("Bytes used for serialization: %d", os.bytes_written);
 
     log_d("After serialization: %d", ESP.getFreeHeap());
 
-    // for (uint16_t i = 0; i < os.bytes_written; i++)
-    // {
-    //     if (buffer[i] < 0x10)
-    //     {
-    //         DEBUG_PRINT(0);
-    //     }
-    //     DEBUG_PRINT(buffer[i], HEX);
-    // }
-    // DEBUG_PRINTLN();
+    for (uint16_t i = 0; i < os.bytes_written; i++) {
+        Serial.print(buffer[i], HEX);
+    }
+    Serial.println();
 
     snappy_env env;
     snappy_init_env(&env);
